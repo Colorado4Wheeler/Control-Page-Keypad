fa90da235d754be5be269401a7fdf43fe50c3f2d
Colorado Four Wheeler <cshortreed@CRAIG.local>
2018-07-05 17:02:05 -0600

    1.2.1
    Removed old update system that polled the Indigo forums!

--- EPS Control Page Keypad.indigoPlugin/Contents/Server Plugin/plugin.py
+++ EPS Control Page Keypad.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -1404,6 +1404,8 @@ class Plugin(indigo.PluginBase):
 	################################################################################
 
 	def updateCheck (self, onlyNewer = False, force = True):
+		return
+		
 		try:
 			try:
 				if self.pluginUrl == "": 
